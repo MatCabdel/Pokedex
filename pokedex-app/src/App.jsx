66d8a511ceb3752cce73c09c2348280d70575000
Mathieu Chauveau <chauveau.math@gmail.com>
2023-10-10 16:52:00 +0200

    suivant et précedent fonctionne mais pas fini

--- pokedex-app/src/App.jsx
+++ pokedex-app/src/App.jsx
@@ -1,24 +1,54 @@
+import { useState } from "react";
 import PokemonCard from "./components/PokemonCard"
 
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const handleClick = () => {
+    if (pokemonIndex < pokemonList.length +1)
+    setPokemonIndex(pokemonIndex + 1);
+  }
+  const lessClick = () => {
+    if (pokemonIndex > 0)
+    setPokemonIndex(pokemonIndex - 1);
+  }
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
-    </div>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      <button onClick={lessClick}>Précedent</button>
+      <button onClick={handleClick}>Suivant</button>
+  </div>
   );
+
 }
 
 
