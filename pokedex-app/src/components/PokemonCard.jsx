230470adb950b3929898e84abe54e4ae9c676c8d
Mathieu Chauveau <chauveau.math@gmail.com>
2023-10-10 10:46:22 +0200

    quete 3 react ok

--- pokedex-app/src/components/PokemonCard.jsx
+++ pokedex-app/src/components/PokemonCard.jsx
@@ -1,10 +1,30 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+
 function PokemonCard () {
+    const pokemon = pokemonList[0];
+
     return (
+    <div>
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbizarre" />
-            <figcaption>Bulbizarre</figcaption>
+            
+                {pokemon.imgSrc ? 
+                <img src={pokemon.imgSrc} alt="Bulbi" />
+                : <p>???</p>}
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
-    );
+    </div>
+    )
 }
+
 
 export default PokemonCard;
